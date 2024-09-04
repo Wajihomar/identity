37691d3994b873f6ff54453a08e886adeab0451e
wajih <wajih@DESKTOP-AT0D6LQ>
2024-09-04 13:34:19 +0300

    Add project files.

---
+++ identity/Models/ViewModel/LoginViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace identity.Models.ViewModel
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [MaxLength(50)]
+        [EmailAddress]
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [DataType(DataType.Password)]
+       
+        public string Password { get; set; }
+    public bool RememberMe { get; set; }
+
+    }
+}
---
+++ identity/Models/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace identity.Models.ViewModel
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [MaxLength(50)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+        public string Phone { get; set; }
+        
+    }
+}
